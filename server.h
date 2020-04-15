497564990e9669e77148c7905984f38d4091d18f
Raahul Acharya <raahul_acharya@college.harvard.edu>
2020-04-14 22:04:00 -0400

    Raahul alter plus read

--- server.h
+++ server.h
@@ -7,8 +7,9 @@
 #include <tuple>
 #include <string>
 
-#define DEFAULT_BUFFER_SIZE 4 //Defines number of ints in the Main Buffer
+#define DEFAULT_BUFFER_SIZE 4 //Defines number of ints in the Main Buffer aka Main Memory
 #define T 2 // Size ratio between components
+
 
 using namespace std;
 
@@ -19,6 +20,7 @@ typedef struct component {
   component* next_component;
   int level;
   int component_number;
+  int element_count;
 } component;
 
 
@@ -31,7 +33,8 @@ typedef struct LSM_Tree {
 //Declarations for server.cpp
 void create(string db_name);
 void load(string path);
-void read(int key);
+int read(int key);
 void write(int key, int value);
 void del(int key);
-void read_data_to_LSM(component* curr_comp);
+int read_data_to_LSM(component* curr_comp);
+int binarySearch(vector<int> *data, int l, int r, int x); 
